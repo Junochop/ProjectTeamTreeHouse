4edd1be99b523f552fedcf09f02e8e00cd8599a8
Junochop <lejuno80@gmail.com>
2017-06-27 08:21:35 -0500

    Add files via upload

---
+++ js/qsource.js
@@ -0,0 +1,33 @@
+//array of quotes
+var quotes = [
+{
+ quote: "You can't handle the truth!",
+ source: "Col. Nathaniel R. Jessep",
+ citation: "A Few Good Men",
+ year: "1992"
+},
+{
+ quote: "The cure for boredom is curiosity. There is no cure for curiosity.",
+ source: "Ellen Parr",
+ citation: " ",
+ year: " "
+},
+{
+ quote: "Always forgive your enemies; nothing annoys them so much.",
+ source: "Oscar Wilde",
+ citation: " ",
+ year: " "
+}, 
+{
+ quote: "Do, or do not. There is no try.",
+ source: "Yoda",
+ citation: "The Empire Strikes Back",
+ year: "1980"
+},
+{
+ quote: "They call it a Royale with cheese.",
+ source: "Vincent Vega",
+ citation: "Pulp Fiction",
+ year: "1994"
+}
+];
---
+++ js/script.js
@@ -0,0 +1,43 @@
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+var message = '';
+
+
+//to generate a random quote number
+function getRandomQuote() {
+var randomNumber = Math.floor(Math.random() * quotes.length);
+return randomNumber;
+}
+
+//to print a random quote
+function printQuote() {
+    var div = document.getElementById("quote-box");
+    var quotesRandom =quotes[getRandomQuote()]; 
+  
+    message = '<p class="quote">' + quotesRandom.quote + '</p>';
+    message += '<p class="source">' + quotesRandom.source + '</p>';
+    
+    //if 'citation' is not an empty string, it will print 'citation'
+    if (quotesRandom.citation !== " ") {   
+      message += '<span class="citation">' + quotesRandom.citation + '</span>';
+
+    //if 'year' is not an empty string, it will print 'year'
+    }  if (quotesRandom.year !== " ") {
+         message += '<span class="year">' + quotesRandom.year + '</span>'; 
+    }
+    
+    div.innerHTML =  message;
+
+    }  
+
+
+
+
+
+
+
+
+
+
